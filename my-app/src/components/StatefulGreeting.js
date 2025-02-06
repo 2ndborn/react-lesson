cde60647b8895a9229c7eb8ef3d2b2d538d2fd54
2ndborn <micah.francis69@googlemail.com>
2025-02-06 13:41:33 +0000

    Class component on statefulgreeting

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            introduction: "Hello!!!",
+            buttonText: "Exit"
+        }
+    }
     render() {
-        return <h2>Hello, {this.props.greeting}</h2>
+        return (
+            <div>
+                <h2>{this.state.introduction}, {this.props.greeting}</h2>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
     }
 }
 
