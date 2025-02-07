d315c8467158959e93d645f56e487cb7a94896f9
2ndborn <micah.francis69@googlemail.com>
2025-02-07 08:19:38 +0000

    Added increment count to Stateful greeting

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -5,16 +5,25 @@ class StatefulGreeting extends React.Component {
         super(props)
         this.state = {
             introduction: "Hello!!!",
-            buttonText: "Exit"
+            buttonText: "Exit",
+            count: 0,
         }
     }
 
     handleClick() {
         this.setState({
-            introduction: "Goodbye!",
-            buttonText: "Enter!",
+            introduction: this.state.introduction === "Hello" ? "Goodbye" : "Hello",
+            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
         })
         console.log(this.state.introduction)
+    }
+
+    increment() {
+        this.setState({
+            count: this.state.count + 1
+        }, ()=> {
+            console.log("New state", this.state.count)
+        })
     }
 
     render() {
@@ -22,6 +31,8 @@ class StatefulGreeting extends React.Component {
             <div>
                 <h2>{this.state.introduction}, {this.props.greeting}</h2>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>increment</button>
+                <p>You have click {this.state.count} times!</p>
             </div>
         )
     }
