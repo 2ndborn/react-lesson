1d316d7229ae77a36e27ba62d373669c6a77085b
2ndborn <micah.francis69@googlemail.com>
2025-02-08 08:20:18 +0000

    Added the e7 snippet

---
+++ my-app/src/components/E7Snippet.js
@@ -0,0 +1,43 @@
+// import React from 'react'
+
+// function E7Snippet() {
+//   return (
+//     <div>E7Snippet</div>
+//   )
+// }
+
+// export default E7Snippet
+
+// import React from 'react'
+
+// const E7Snippet = () => {
+//   return (
+//     <div>E7Snippet</div>
+//   )
+// }
+
+// export default E7Snippet
+
+// import React, { Component } from 'react'
+
+// export class E7Snippet extends Component {
+//   render() {
+//     return (
+//       <div>E7Snippet</div>
+//     )
+//   }
+// }
+
+// export default E7Snippet
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// console.log(first)
+
+// console.log('first', first)
