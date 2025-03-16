cb535d3c12f3a19f3b934bb8742aa538b9389837
2ndborn <micah.francis69@googlemail.com>
2025-03-16 13:57:57 +0000

    Rendered the json data to the browser

--- my-app/src/components/HttpRequests.js
+++ my-app/src/components/HttpRequests.js
@@ -12,10 +12,19 @@ export class HttpRequests extends Component {
 
     componentDidMount() {
       axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response)
+        this.setState({
+          post: response.data,
+        })
+      })
     }
   render() {
     return (
-      <div>HttpRequests</div>
+      <div>
+        <h2>Posts:</h2>
+        {JSON.stringify(this.state.post)}
+      </div>
     )
   }
 }
