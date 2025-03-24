fa3febbe1214f3e1bbf603599ad67c60e351bc37
2ndborn <micah.francis69@googlemail.com>
2025-03-24 19:31:47 +0000

    Added the search bar

--- my-app/src/components/MyRequestHooks.js
+++ my-app/src/components/MyRequestHooks.js
@@ -16,6 +16,7 @@ function MyRequestHooks() {
         const response = await axios.get(`https://pixabay.com/api/videos/?key=${API_KEY}&per_page=5&video_type=animation`)
         console.log(response)
         const fetchedPosts = response.data.hits;
+        console.log(fetchedPosts)
         setTimeout(() => {
             setIsLoaded(true);
         }, 2000)
@@ -23,8 +24,31 @@ function MyRequestHooks() {
         setSavedPosts(fetchedPosts);
     }
 
+    const handleChange = (e) => {
+        // Get the input value from the search bar and convert it to lowercase for case-insensitive matching
+        const name = e.target.value.toLowerCase();
+    
+        // Filter the savedPosts array to find posts where the 'user' field matches the input
+        const filteredPosts = savedPosts.filter(post => {
+            return post.user.toLowerCase().includes(name);
+        });
+    
+        // Update the 'posts' state with the filtered results
+        setPosts(filteredPosts);
+    };
+
   return (
     <div>
+        <form>
+            <label htmlFor='searchinput'>Search</label>
+            <input 
+            type='search'
+            id='searchinput'
+            onChange={(e) => handleChange(e)}
+            placeholder='Search'
+            />
+        </form>
+        <h3>Post: {posts.length}</h3>
         {isLoaded ? (
             <MyPostAPI savedPosts={posts} />
         
